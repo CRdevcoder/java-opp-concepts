2fad831b17ad9ebf374930f450ad14f1891d177b
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-13 15:48:21 -0400

    Created MergeSort Class (Not implemented yet)

---
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/MergeSort.java
@@ -0,0 +1,15 @@
+package sortingalgorithms;
+
+import java.util.ArrayList;
+
+public class MergeSort implements Sorter {
+
+    public <T extends Comparable<T>> ArrayList<T> sortList( ArrayList<T> listArg)
+    {
+        // Implement Merge Sort Here.
+        return listArg;
+    }
+
+
+
+}
