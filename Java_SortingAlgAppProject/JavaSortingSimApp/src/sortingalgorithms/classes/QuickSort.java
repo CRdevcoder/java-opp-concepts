e5ea24cb2daca92131eb1a78936e8d19f48bf013
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-09-03 09:17:32 -0400

    New QuickSort class

---
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/classes/QuickSort.java
@@ -0,0 +1,17 @@
+package sortingalgorithms.classes;
+
+import java.util.ArrayList;
+
+public class QuickSort implements Sorter {
+
+    @Override
+    // calls quicksort algorithm.
+    public <T extends Comparable<T>> ArrayList<T> sortList(ArrayList<T> list) {
+        return null; // UNIMPLEMENTED
+    }
+
+    
+
+
+
+}
